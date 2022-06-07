8b8fb08cc3df2e7f18c7afbd2456849aa17ecea4
heungseop <imt300@naver.com>
2022-06-08 00:00:52 +0900

    react study

---
+++ react-for-beginers/src/6.2Deps App.js
@@ -0,0 +1,40 @@
+import Button from "./Button";
+import styles from "./App.module.css";
+import { useState, userEffect, useEffect } from "react";
+
+function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (e) => setKeyword(e.target.value);
+  // console.log("i run all the time");
+
+  useEffect(() => {
+    console.log("i run only once.");
+  }, []);
+
+  useEffect(() => {
+    console.log("i run when 'keyword' changes.");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("i run when 'counter' changes.");
+  }, [counter]);
+  useEffect(() => {
+    console.log("i run when 'keyword' and 'counter' changes.");
+  }, [keyword, counter]);
+
+  return (
+    <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
+      <h1 className={styles.title}>{counter}</h1>
+      <button onClick={onClick}>click me </button>
+    </div>
+  );
+}
+
+export default App;
